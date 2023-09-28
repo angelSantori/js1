70bd6a0437aa10ae462010b6c93b8577610650e0
angelSantori <antonyo.da.mm@gmail.com>
2023-09-28 10:09:02 -0700

    Rename

--- img/hola_-mundo.js
+++
@@ -1,4 +0,0 @@
-'use strict' //Modo estricto para programar con buenas prácticas
-
-//Imprimir en pantalla
-console.log('Hola mundo desde Node.js, esto se verá en la terminal de comandos')
---
+++ img/hola_mundo.js
@@ -0,0 +1,4 @@
+//Imprimir en pantalla
+console.log('Hola mundo desde Node.js, esto se verá en la terminal de comandos')
+
+//Esto es backend
