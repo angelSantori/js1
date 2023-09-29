9d8d32e11782ac17d0f99d9b5d85f4d6e6d3700e
angelSantori <antonyo.da.mm@gmail.com>
2023-09-29 09:47:15 -0700

    Lectura con php

---
+++ leer.php
@@ -0,0 +1,15 @@
+<?php
+printf("Abriendo archivo");
+
+$archivo_url = fopen("el_quijote.txt", "r");
+
+$texto = "";
+
+while ($fragmento = fgets($archivo_url)) {
+    $texto .= $fragmento;
+}
+
+printf($texto);
+printf("<br> Haciendo otra cosa");
+
+?>
