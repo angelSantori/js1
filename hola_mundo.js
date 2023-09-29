0b155f70f448230afa4df4b2e47435d267ea248d
angelSantori <antonyo.da.mm@gmail.com>
2023-09-29 09:25:14 -0700

    Instruccion frontend

--- hola_mundo.js
+++ hola_mundo.js
@@ -5,3 +5,6 @@ console.log('Hola mundo desde Node.js, esto se verá en la terminal de comandos'
 
 //Operación básica
 console.log(2*5)
+
+//Al utilizar una instrucción de JS frontend marca error en node js
+console.log(window)
