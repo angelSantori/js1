7b8492da09b13b91caadedc807e76cc27d7c8511
angelSantori <antonyo.da.mm@gmail.com>
2023-10-09 09:43:47 -0700

    Archivo server_http.js

---
+++ server_http.js
@@ -0,0 +1,18 @@
+// Llamamos el modulo http
+const http = require('http');
+const hostname = '192.168.1.115'; //IP del servidor
+const port = 3000; // Buena practica del 3000 hacia arriba para no interferir 
+
+const server = http.createServer((req,res)=> {
+
+    res.statusCode = 200; // Codigo http 200 si todo sale bien -> mandara cabecera
+    res.setHeader ('Content-Type', 'text/html');
+    res.end('<h1>Hola mundo</h1>');
+
+});
+
+server.listen(port, hostname, ()=>{
+
+    console.log('El servidor esta corriendo en http://${hostname}:${port}/');
+
+});
