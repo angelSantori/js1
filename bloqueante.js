d2ec5e7395251d202993c508b284d217b5f60cd9
angelSantori <antonyo.da.mm@gmail.com>
2023-10-19 13:19:03 -0700

    Correción, se completo readFileSync

--- bloqueante.js
+++ bloqueante.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 console.log("\nAbriendo el archivo");
 
-var content = fs.readFile("el_quijote.txt");
+var content = fs.readFileSync("el_quijote.txt");
 console.log(content);
 
 console.log("\nHaciendo otra cosa");
